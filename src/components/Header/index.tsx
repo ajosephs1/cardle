63c93332c303376742010879f6038f68ef8ab6ad
ajosephs1 <77748724+ajosephs1@users.noreply.github.com>
2023-08-01 22:12:03 -0400

    Add basic header structure

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,27 @@
-import './Header.scss'
+import bbLogo from "../../assets/images/boxbox_logo.svg";
+import instagram from "../../assets/icons/instagram-icon.svg";
+import question from "../../assets/icons/question-icon.svg";
+import chart from "../../assets/icons/chart-icon.svg";
+import "./Header.scss";
 
-export default function Header(){
-return(
-    <h1>HEADER</h1>
-)
+export default function Header() {
+  return (
+    <nav className="nav">
+      <a href="https://www.boxbox.autos/" className="nav__logo">
+        <img src={bbLogo} alt="BoxBox logo" className="nav__logo--img" />
+      </a>
+      <h1 className="nav__header">CAR-DLE</h1>
+      <div className="nav__links">
+        <div className="nav__link">
+          <img src={instagram} alt="instagram icon" className="nav__icon" />
+        </div>
+        <div className="nav__link">
+          <img src={question} alt="question icon" className="nav__icon" />
+        </div>
+        <div className="nav__link">
+          <img src={chart} alt="chart icon" className="nav__icon" />
+        </div>
+      </div>
+    </nav>
+  );
 }
