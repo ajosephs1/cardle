a8acf2e84cb931ca46c219515dc196e383ab1caf
ajosephs1 <77748724+ajosephs1@users.noreply.github.com>
2023-08-04 22:26:32 -0400

    Material UI

--- src/components/FormSubmit/index.tsx
+++ src/components/FormSubmit/index.tsx
@@ -1,11 +1,17 @@
-import Select from '../Select'
-import './FormSubmit.scss'
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import InputLabel from "@mui/material/InputLabel";
+import "./FormSubmit.scss";
 
-export default function FormSubmit(){
-
-    return(
-        <form action="" className="formsubmit">
-            <Select/>
-        </form>
-    )
+export default function FormSubmit() {
+  return (
+    <form action="" className="formsubmit">
+      <InputLabel id="formSelect">Car</InputLabel>
+      <Select label="Car" className="form__select" id="formSelect">
+        <MenuItem value={10}>Ten</MenuItem>
+        <MenuItem value={20}>Twenty</MenuItem>
+        <MenuItem value={30}>Thirty</MenuItem>
+      </Select>
+    </form>
+  );
 }
