aa590a0ca8242faa36b93ed1231143f7f8d41e23
ajosephs1 <77748724+ajosephs1@users.noreply.github.com>
2023-08-18 12:12:21 -0400

    Add light functionality

--- src/components/ScoreBoard/index.tsx
+++ src/components/ScoreBoard/index.tsx
@@ -4,16 +4,23 @@ type ScoreBoardProps = {
   currentRound: number;
   multiplier: number;
   currentPoints: number;
+  score: {
+    [key: number]: {
+      make: null | boolean;
+      model: null | boolean;
+      year: null | boolean;
+    };
+  };
 };
 
 // add animation delay for each light 1s
-// 
+//
 export default function ScoreBoard({
   currentRound,
   multiplier,
   currentPoints,
+  score,
 }: ScoreBoardProps) {
-
   return (
     <section className="scoreboard">
       <div className="scoreboard__headings">
@@ -22,60 +29,150 @@ export default function ScoreBoard({
         <h2 className="scoreboard__heading">Year</h2>
       </div>
       {/* each light will be given class name depending on value for respective round add delay appropriately for each light 1s 2s 3s */}
-      <section className="scoreboard__light-container">
+      <section className="scoreboard__light-container ">
         <div className="scoreboard__row">
           <div className="scoreboard__light__container">
-            <div className="scoreboard__light" />
+            <div
+              className={`scoreboard__light scoreboard__light--make ${
+                score[1].make
+                  ? "scoreboard__light--green"
+                  :(score[1].make === null? "" : "scoreboard__light--red")
+              }`}
+            />
           </div>
           <div className="scoreboard__light__container">
-            <div className="scoreboard__light" />
+          <div
+              className={`scoreboard__light scoreboard__light--model ${
+                score[1].model
+                  ? "scoreboard__light--green"
+                  :(score[1].model === null? "" : "scoreboard__light--red")
+              }`}
+            />
           </div>
           <div className="scoreboard__light__container">
-            <div className="scoreboard__light" />
+          <div
+              className={`scoreboard__light scoreboard__light--year ${
+                score[1].year
+                  ? "scoreboard__light--green"
+                  :(score[1].year === null? "" : "scoreboard__light--red")
+              }`}
+            />
           </div>
         </div>
         <div className="scoreboard__row">
           <div className="scoreboard__light__container">
-            <div className="scoreboard__light" />
+            <div
+              className={`scoreboard__light scoreboard__light--make ${
+                score[2].make
+                  ? "scoreboard__light--green"
+                  :(score[2].make === null? "" : "scoreboard__light--red")
+              }`}
+            />
           </div>
           <div className="scoreboard__light__container">
-            <div className="scoreboard__light" />
+          <div
+              className={`scoreboard__light scoreboard__light--model ${
+                score[2].model
+                  ? "scoreboard__light--green"
+                  :(score[2].model === null? "" : "scoreboard__light--red")
+              }`}
+            />
           </div>
           <div className="scoreboard__light__container">
-            <div className="scoreboard__light" />
-          </div>
-        </div>
-        <div className="scoreboard__row">
-          <div className="scoreboard__light__container">
-            <div className="scoreboard__light" />
-          </div>
-          <div className="scoreboard__light__container">
-            <div className="scoreboard__light" />
-          </div>
-          <div className="scoreboard__light__container">
-            <div className="scoreboard__light" />
-          </div>
-        </div>
-        <div className="scoreboard__row">
-          <div className="scoreboard__light__container">
-            <div className="scoreboard__light" />
-          </div>
-          <div className="scoreboard__light__container">
-            <div className="scoreboard__light" />
-          </div>
-          <div className="scoreboard__light__container">
-            <div className="scoreboard__light" />
+          <div
+              className={`scoreboard__light scoreboard__light--year ${
+                score[2].year
+                  ? "scoreboard__light--green"
+                  :(score[2].year === null? "" : "scoreboard__light--red")
+              }`}
+            />
           </div>
         </div>
         <div className="scoreboard__row">
           <div className="scoreboard__light__container">
-            <div className="scoreboard__light" />
+            <div
+              className={`scoreboard__light scoreboard__light--make ${
+                score[3].make
+                  ? "scoreboard__light--green"
+                  :(score[3].make === null? "" : "scoreboard__light--red")
+              }`}
+            />
           </div>
           <div className="scoreboard__light__container">
-            <div className="scoreboard__light" />
+          <div
+              className={`scoreboard__light scoreboard__light--model ${
+                score[3].model
+                  ? "scoreboard__light--green"
+                  :(score[3].model === null? "" : "scoreboard__light--red")
+              }`}
+            />
           </div>
           <div className="scoreboard__light__container">
-            <div className="scoreboard__light" />
+          <div
+              className={`scoreboard__light scoreboard__light--year ${
+                score[3].year
+                  ? "scoreboard__light--green"
+                  :(score[3].year === null? "" : "scoreboard__light--red")
+              }`}
+            />
+          </div>
+        </div>
+        <div className="scoreboard__row">
+          <div className="scoreboard__light__container">
+            <div
+              className={`scoreboard__light scoreboard__light--make ${
+                score[4].make
+                  ? "scoreboard__light--green"
+                  :(score[4].make === null? "" : "scoreboard__light--red")
+              }`}
+            />
+          </div>
+          <div className="scoreboard__light__container">
+          <div
+              className={`scoreboard__light scoreboard__light--model ${
+                score[4].model
+                  ? "scoreboard__light--green"
+                  :(score[4].model === null? "" : "scoreboard__light--red")
+              }`}
+            />
+          </div>
+          <div className="scoreboard__light__container">
+          <div
+              className={`scoreboard__light scoreboard__light--year ${
+                score[4].year
+                  ? "scoreboard__light--green"
+                  :(score[4].year === null? "" : "scoreboard__light--red")
+              }`}
+            />
+          </div>
+        </div>
+        <div className="scoreboard__row">
+          <div className="scoreboard__light__container">
+            <div
+              className={`scoreboard__light scoreboard__light--make ${
+                score[5].make
+                  ? "scoreboard__light--green"
+                  :(score[5].make === null? "" : "scoreboard__light--red")
+              }`}
+            />
+          </div>
+          <div className="scoreboard__light__container">
+          <div
+              className={`scoreboard__light scoreboard__light--model ${
+                score[5].model
+                  ? "scoreboard__light--green"
+                  :(score[5].model === null? "" : "scoreboard__light--red")
+              }`}
+            />
+          </div>
+          <div className="scoreboard__light__container">
+          <div
+              className={`scoreboard__light scoreboard__light--year ${
+                score[5].year
+                  ? "scoreboard__light--green"
+                  :(score[5].year === null? "" : "scoreboard__light--red")
+              }`}
+            />
           </div>
         </div>
       </section>
@@ -84,7 +181,9 @@ export default function ScoreBoard({
           <p className="scoreboard__text scoreboard__text--current">
             Current Points:
           </p>
-          <p className="scoreboard__text scoreboard__text--current">{currentPoints === 0 ? `-`: currentPoints}</p>
+          <p className="scoreboard__text scoreboard__text--current">
+            {currentPoints === 0 ? `-` : currentPoints}
+          </p>
         </div>
         <div className="scoreboard__tally-row">
           <p className="scoreboard__text">Round Multiplier</p>
@@ -94,7 +193,11 @@ export default function ScoreBoard({
           <p className="scoreboard__text scoreboard__text--total">
             Total Points:
           </p>
-          <p className="scoreboard__text scoreboard__text--total">{(currentPoints * multiplier) === 0 ? `-`: currentPoints * multiplier}</p>
+          <p className="scoreboard__text scoreboard__text--total">
+            {currentPoints * multiplier === 0
+              ? `-`
+              : currentPoints * multiplier}
+          </p>
         </div>
       </section>
     </section>
